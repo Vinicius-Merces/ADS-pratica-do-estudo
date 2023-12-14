a3fd140768955f96887cfed70041178ec0959b8d
Vinicius-Merces <92902670+Vinicius-Merces@users.noreply.github.com>
2023-12-13 23:55:30 -0300

    Teste com botões em pais e filhos

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -9,10 +9,12 @@ function App() {
   <div className='App'>
     <h1>Testando Eventos</h1>
     <Evento numero={"1"}/>
-    <Evento numero={"2"}/>
+    
     <Form />
   </div>
   );
 }
 
 export default App;
+
+
--- React/my-app/src/components/Eventos.js
+++ React/my-app/src/components/Eventos.js
@@ -1,12 +1,21 @@
-function Evento({ numero }) {
+import Button from './Button'
+
+function Evento() {
 
     function meuEvento(){
-        console.log(`Opa, fui ativado! ${numero}`);
+        console.log(`Ativando primeiro evento`);
     }
+
+    function segundoEvento() {
+        console.log(`Ativando segundo evento!`)
+    }
+
     return (
         <div>
             <p>Clique para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button event={meuEvento} text="Primeiro Evento" />
+            <Button event={segundoEvento} text="Segundo evento"/>
+            
         </div>
     )
 }
